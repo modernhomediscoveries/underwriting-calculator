d5295dce4dbb63cf3369435cd3c274a702e2039d
EstateGather <152664707+modernhomediscoveries@users.noreply.github.com>
2024-11-04 18:55:06 -0500

    green dot

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -18,7 +18,7 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-neutral-100 dark:bg-neutral-800">
       <SliderPrimitive.Range className="absolute h-full bg-primary_green dark:bg-neutral-50" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary_green bg-white ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary_green focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-50 dark:bg-primary_green dark:ring-offset-primary_green dark:focus-visible:ring-neutral-300" />
+     <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary_green bg-primary_green ring-offset-primary_green transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary_green focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-50 dark:bg-primary_green dark:ring-offset-primary_green dark:focus-visible:ring-neutral-300" />
   </SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
